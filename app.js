f280fb31f1df633f1c18249762917d65b67f7ba8
estherryu77 <estherryu77@gmail.com>
2021-08-03 11:50:53 -0600

    js problems

--- app.js
+++ app.js
@@ -4,41 +4,33 @@ const API_KEY = 'e804b9f68f3945eb901463f2b88baf78'
 
 
 
-// create dynamic dropdown menu
-// make request and concert response to dropdown menu 
+// create dynamic dropdown menu by type of wine
+//create text search menu by wine name 
 // hardcode selection list for drop-down menu: white wine, red wine, dessert wine, rose wine, sparkling wine, sherry, vermouth, fruit wine, and mead.
 
 const selectTag = document.querySelector('#select-wine')
 const form = document.querySelector('form')
 const dataContainer = document.querySelector('#wine-results')
 const searchButton = document.querySelector('#button1')
-const secondButton = document.querySelector('#button2')
-// const wineSelect = document.querySelector('#select-wine')
+const getResults = document.querySelector('#button2')
+
 // console.log(form)
 // console.log(button)
-// const wineList = ["white wine, red wine, dessert wine, rose wine, sparkling wine, sherry, vermouth, fruit wine, mead"]
+
 
 const getAllWines = async () => {
   try {
-    // let wineType = 'cabernet sauvignon'
+    
     const wineSearchValue = document.querySelector('#wine-search').value
-    console.log(wineSearchValue)
+    // console.log(wineSearchValue)
     // const wineURL = `https://api.spoonacular.com/food/wine/recommendation?wine=${wineType}&number=10&apiKey=e804b9f68f3945eb901463f2b88baf78`
     const wineURL = `${DOMAIN}?wine=${wineSearchValue}&number=10&apiKey=${API_KEY}`
     const response = await axios.get(wineURL)
-    console.log(response.data.recommendedWines)
-      // .then(res => {
-      //   console.log(res.data)
-      // })
+    // console.log(response.data.recommendedWines)
+      
+    const wineList = Object.keys(response.data.recommendedWines)
 
-
-    // console.log(responseObject)
-    // object.keys returns an array from winerecommendations list in API Docs
-    // const wineList = Object.keys(response.data.message)
-    // console.log(wineList)
-
-    return response
-    // return wineList
+    return wineList
   } catch (error) {
     console.error(error)
   }
@@ -47,8 +39,6 @@ searchButton.addEventListener('click', (e) => {
   e.preventDefault()
   getAllWines() 
 })
-// getAllWines()
-
 
 const getWineType = async () => {
   try {
@@ -75,12 +65,75 @@ const getWineType = async () => {
     console.error(error)
   }
 }
-secondButton.addEventListener('click', (e) => {
+getResults.addEventListener('click', (e) => {
   e.preventDefault()
-  getWineType() 
+  getWineType()
+ 
+  
 })
 
-// create form option tags from request
+function renderList(wineList) {
+  console.log(wineList)
+  
+  let section = document.querySelector('wineItems')
+  wineList.forEach((wine) => {
+    let wineStats = document.createElement('div')
+    section.append(wineStats)
+
+    //wine name
+    let wineTitle = document.createElement('h2')
+    wineTitle.textContent = `${data.recommendedWines.title}`
+    wineStats.append(wineTitle)
+
+    //wine Image
+
+    let wineImg = document.createElement('img')
+    wineImg.src = `${data.recommendedWines.title}`
+    wineStats.append(wineTitle)
+
+    //wine score
+    let wineScore = document.createElement('h3')
+    wineScore.textContent = `${data.recommendedWines.score}`
+    wineStats.append(wineScore)
+
+    //wine description
+    let wineDesc = document.createElement('p')
+    wineDesc.textContent = `${data.recommendedWines.description}`
+    wineStats.append(wineDesc)
+
+    // wine price
+    let winePrice = document.createElement('h5')
+    winePrice.textContent = `${data.recommendedWines.price}`
+    wineStats.append(winePrice)
+    })
+
+}
+
+// render wine Data
+
+const getData = async () => {
+  try {
+    const wineInput =
+      document.querySelector('input').value
+    console.log(wineInput)
+    const data = await axios.get(`https://api.spoonacular.com/food/wine/recommendation?wine=${wineInput}&number=10&apiKey=e804b9f68f3945eb901463f2b88baf78`)
+    console.log(data.data)
+    return data
+    
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
