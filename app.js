4e6609c210515c25f19b271b50c795a19e5d58af
estherryu77 <estherryu77@gmail.com>
2021-08-05 12:45:01 -0600

    cleaned up code

--- app.js
+++ app.js
@@ -1,27 +1,4 @@
-// let recommendedWines = [
-//   {
-//       "id": 447938,
-//       "title": "Rombauer Merlot",
-//       "averageRating": 0.96,
-//       "description": "Enticing and lively red color; beautifully aromatic with black cherry and ripe plum. On the palate a purity of blackcurrant, cedar and mint flavors blend together seamlessly. Soft and supple, this wine has a medium-bodied mouth-feel with plush tannins that integrate with the generous finish.Our favorite pairings for this wine include chicken parmesan, cedar-planked salmon over wild rice, and mushroom pizza.",
-//       "imageUrl": "https://spoonacular.com/productImages/447938-312x231.jpg",
-//       "link": "https://click.linksynergy.com/deeplink?id=*QCiIS6t4gA&mid=2025&murl=https%3A%2F%2Fwww.wine.com%2Fproduct%2Frombauer-merlot-2008%2F116883",
-//       "price": "$25.59",
-//       "ratingCount": 5.0,
-//       "score": 0.8975
-//   },
-//   {
-//       "id": 430475,
-//       "title": "NV The Big Kahuna Merlot",
-//       "averageRating": 0.9,
-//       "description": "A ripe and rounded Merlot with notes of plum, blackberry, and hint of spice.",
-//       "imageUrl": "https://spoonacular.com/productImages/430475-312x231.jpg",
-//       "link": "https://www.amazon.com/Big-Kahuna-Merlot-Red-Wine/dp/B01F5XPTUW?tag=spoonacular-20",
-//       "price": "$6.99",
-//       "ratingCount": 4.0,
-//       "score": 0.823076923076923
-//   }
-// ]
+
 
 
 const DOMAIN = 'https://api.spoonacular.com/food/wine/recommendation'
@@ -33,19 +10,18 @@ const API_KEY = 'e804b9f68f3945eb901463f2b88baf78'
 
 // create dynamic dropdown menu by type of wine
 //create text search menu by wine name 
-// hardcode selection list for drop-down menu: white wine, red wine, dessert wine, rose wine, sparkling wine, sherry, vermouth, fruit wine, and mead.
 
+// select drop down menu list and put it in a constant
+// select form and put it in a constant
 const selectTag = document.querySelector('#select-wine')
 const form = document.querySelector('form')
 
 
-
+// select submit button and put it in a constant
+// select main div where all the data from api will be appended and put it in const main Data
 const getResults = document.querySelector('#button2')
 const mainData = document.querySelector('main')
-// console.log(mainData)
 
-// console.log(form)
-// console.log(button)
 
 
 const getWineType = async () => {
